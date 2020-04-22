63cfc0bbfa7642d1a62134bc4cefbbb858ee46a6
Diego Pacheco <dipacmon@live.com>
2020-04-22 10:11:55 -0500

    Version 1

---
+++ socketmulticast.cpp
@@ -0,0 +1,59 @@
+#include "socketmulticast.h"
+  SocketMulticast(int)
+  {
+    int reuse = 1;
+    bzero((char *)&direccionForanea, sizeof(direccionForanea));
+  	bzero((char *)&direccionLocal, sizeof(direccionLocal));
+  	if ((s = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+  		perror("Socket creation failed");
+  		exit(EXIT_FAILURE);
+  	}
+
+  	int reuse = 1;
+  	if (setsockopt(s, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) == -1) {
+  		printf("Error al llamar a la funciรณn setsockopt\n");
+  		exit(0);
+  	}
+
+  	direccionLocal.sin_family = PF_INET;
+  	direccionLocal.sin_addr.s_addr = INADDR_ANY;
+  	direccionLocal.sin_port = htons(pto);
+  	if (bind(s, (struct sockaddr *)&direccionLocal, sizeof(direccionLocal)) < 0)
+  	{
+  		perror("bind failed");
+  		exit(EXIT_FAILURE);
+  	}
+  }
+  ~SocketMulticast()
+  {
+    close(s);
+  }
+  int SocketMulticast::recibe(PaqueteDatagrama & p)
+  {
+    socklen_t clilen = sizeof(direccionForanea);
+    int a = recvfrom(s, (char *) p.obtieneDatos(), p.obtieneLongitud(), 0, (struct sockaddr *) &direccionForanea, &clilen);
+    p.inicializaIp(inet_ntoa(direccionForanea.sin_addr));
+    p.inicializaPuerto(ntohs(direccionForanea.sin_port));
+    return a;
+  }
+  int SocketMulticast::envia(PaqueteDatagrama & p, unsigned char ttl)
+  {
+    setsockopt(s, IPPROTO_IP, IP_MULTICAST_TTL, (void*)&ttl, sizeof(ttl));
+    direccionForanea.sin_family = AF_INET;
+  	direccionForanea.sin_addr.s_addr = inet_addr(p.obtieneDireccion());
+  	direccionForanea.sin_port = htons(p.obtienePuerto());
+    return sendto(s, (char *) p.obtieneDatos(), p.obtieneLongitud(), 0, (struct sockaddr *)&direccionForanea, sizeof(direccionForanea));
+
+  }
+  void SocketMulticast::unirseGrupo(char *multicastIP)
+  {
+    multicast.imr_multiaddr.s_addr = inet_addr(multicastIP);
+    multicast.imr_interface.s_addr = htonl(INADDR_ANY);
+    setsockopt(s, IPPROTO_IP, IP_ADD_MEMBERSHIP, (void*)&multicast, sizeof(multicast));
+  }
+  void SocketMulticast::salirseGrupo(char *multicastIP)
+  {
+    multicast.imr_multiaddr.s_addr = inet_addr(multicastIP);
+    multicast.imr_interface.s_addr = htonl(INADDR_ANY);
+    setsockopt(s, IPPROTO_IP, IP_DROP_MEMBERSHIP, (void*)&multicast, sizeof(multicast));
+  }
---
+++ socketmulticast.h
@@ -0,0 +1,15 @@
+#ifndef SOCKETMULTICAST_H
+#define SOCKETMULTICAST_H
+class SocketMulticast{
+  public:
+    SocketMulticast(int);
+    ~SocketMulticast();
+    int recibe(PaqueteDatagrama);
+    int envia(PaqueteDatagrama, unsigned char); //Se une a un grupo multicast, recibe la IP multicast
+    void unirseGrupo(char *);
+    //Se sale de un grupo multicast, recibe la IP multicast
+    void salirseGrupo(char *);
+  private:
+     int s; //ID socket
+ };
+#endif
